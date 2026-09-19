57909b88d2339c6e2bedbd28cf92a338607e679a
Mrvibecodic <junkmailforjunk@protonmail.com>
2026-09-19 13:31:29 +0300

    chore(release): 0.1.21-alpha.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,8 +65,8 @@ subprojects {
             minSdk = 23
             targetSdk = 35
 
-            versionName = "0.1.21-alpha.9"
-            versionCode = 12109
+            versionName = "0.1.21-alpha.10"
+            versionCode = 12110
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
